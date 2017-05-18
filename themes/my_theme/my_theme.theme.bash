39864b02c9ce41f3b6d6f7128a120dfde269c082
AustinXDLiu <scutliuxudong@gmail.com>
2017-05-18 19:51:53 +0800

    Add a theme of my own.

---
+++ themes/my_theme/my_theme.theme.bash
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+SCM_THEME_PROMPT_DIRTY=" ${bold_red}⊘${normal}"
+SCM_THEME_PROMPT_CLEAN=" ${bold_green}✓${normal}"
+SCM_THEME_PROMPT_PREFIX="${reset_color}["
+SCM_THEME_PROMPT_SUFFIX=" ${reset_color}]"
+
+GIT_THEME_PROMPT_DIRTY=" ${bold_red}⊘${normal}"
+GIT_THEME_PROMPT_CLEAN=" ${bold_green}✓${normal}"
+GIT_THEME_PROMPT_PREFIX="${blue}["
+GIT_THEME_PROMPT_SUFFIX=" ${blue}]"
+
+STATUS_THEME_PROMPT_BAD="${red}❯${reset_color}${normal}"
+STATUS_THEME_PROMPT_OK="${green}❯${reset_color}${normal}"
+
+function prompt_command() {
+    local ret_status="$( [ $? -eq 0 ] && echo -e "$STATUS_THEME_PROMPT_OK" || echo -e "$STATUS_THEME_PROMPT_BAD")"
+    PS1="${bold_black}\u${black}@${bold_black}\h $(scm_prompt_info)\n${black}\w ${ret_status} "
+}
+
+safe_append_prompt_command prompt_command
